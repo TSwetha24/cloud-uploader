08875a6951f452915ba0082cce17eabfb34612f0
TSwetha24 <swethakavitha2426@gamil.com>
2025-06-30 23:53:58 +0530

    Initial commit - Cloud File Uploader

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+        return render_template('success.html', filename=file.filename)
+    return "No file selected."
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
